1b17575ada218410d9830c5092be488c2bfc5ce1
gomedun <jesusmlopezf@me.com>
2014-05-02 03:18:06 -0700

    EXPLOSIONESgit add .git add .

---
+++ explosion.lua
@@ -0,0 +1,60 @@
+explosion = {}
+explosion.radius = 50
+explosion.timer = 1
+
+
+function explosion.spawn(x, y)
+	table.insert(explosion, {x=x, y=y, time = 0, indivRadius = 0})
+end
+
+function explosionUpdate(v, dt)
+	v.indivRadius = v.indivRadius + 50*dt
+	if v.time <= 1 then
+		v.time=v.time+dt
+	else
+	    v=nil
+	end
+end
+
+function explosionDraw(v)
+	local colory=math.random(225)
+	love.graphics.setColor(colory+30,colory,0)
+	love.graphics.circle("fill",v.x,v.y,v.indivRadius)
+end
+
+function explosion.update(dt)
+	for i,v in ipairs(explosion) do 
+		explosionUpdate(v, dt)
+		if v.time>=explosion.timer then
+			table.remove(explosion, i)
+		end
+	end
+	explosion.collision()
+end
+
+function explosion.draw()
+	for i,v in ipairs(explosion) do 
+		explosionDraw(v)
+	end
+	for p,q in ipairs(explosion) do
+		for i,v in ipairs(enemy) do 
+			love.graphics.print(math.sqrt(((v.x-q.x)^2)+((v.y-q.y)^2)),50,200)
+		end 
+		love.graphics.print(math.sqrt(math.pow(player.x-q.x, 2)+math.pow(player.y-q.y,2)),50,400)
+	end
+end
+
+function explosion.collision()
+	for p,q in ipairs(explosion) do
+		for i,v in ipairs(enemy) do 
+			if math.sqrt((((v.x+enemy.width/2)-q.x)^2)+(((v.x+enemy.height/2)-q.y)^2))<(enemy.width/2)+q.indivRadius then 
+				table.remove(enemy, i)
+			end 
+		end 
+		if math.sqrt(math.pow((player.x+player.width/2)-q.x, 2)+math.pow((player.y+player.height/2)-q.y,2))<(player.width/2)+q.indivRadius and not player.invincible then 
+			player.hp = player.hp -1
+			player.invTimer = 0
+			player.invincible = true
+		end 
+	end
+end
