7a142e76b60a2456bfb45d90732f3729c7c1eb3e
gomedun <jesusmlopezf@me.com>
2014-05-07 23:14:33 -0700

    Introgit add .!

--- intro.lua
+++ intro.lua
@@ -1,24 +1,28 @@
 intro={}
 intro.framecount=0
+intro.cheve={}
 
 function intro.load()
 	intro.framecount=0.00000000001
-	for i=0,119,1 do 
-		cheve[i+1]=love.graphics.newImage("img/intro/Sequence 01"..tostring(i)..".jpg")
+	for i=0,119,1 do
+		if i >99 then
+			intro.cheve[i+1]=love.graphics.newImage("img/intro/Sequence 01"..tostring(i)..".jpg")
+		elseif i==0 then
+			intro.cheve[i+1]=love.graphics.newImage("img/intro/Sequence 0100"..tostring(i)..".jpg")
+		elseif i<10 then
+			intro.cheve[i+1]=love.graphics.newImage("img/intro/Sequence 0100"..tostring(i)..".jpg")
+		elseif i<100 then
+			intro.cheve[i+1]=love.graphics.newImage("img/intro/Sequence 010"..tostring(i)..".jpg")
+		end
 	end
 end
 
 function intro.update(dt)
-	if intro.framecount>600 then
-		Gamestate.switch(menu)
-	else
-		intro.framecount=5*dt
-	end
+
 end
 
 function intro.draw()
-	frame = math.ceil(intro.framecount/5)-1
-	love.graphics.draw(cheve[frame],0,0)
+
 end
 
 function intro:init()
@@ -30,11 +34,16 @@ function intro:enter()
 end
 
 function intro:update(dt)
-	intro.update(dt)
+	if intro.framecount>590 then
+		Gamestate.switch(menu)
+	else
+		intro.framecount=intro.framecount+150*dt
+	end
 end
 
 function intro:draw()
-	intro.draw()
+	frame = math.ceil(intro.framecount/5)
+	love.graphics.draw(intro.cheve[frame],0,0)
 end 
 
 function intro:keypressed()
