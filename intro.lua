d768e6dddafe58f3d5b72c28b30fca415a3ca16c
gomedun <jesusmlopezf@me.com>
2014-05-08 00:01:12 -0700

    Fix memory leak

--- intro.lua
+++ intro.lua
@@ -1,20 +1,9 @@
 intro={}
 intro.framecount=0
-intro.cheve={}
+intro.cheve=nil
 
 function intro.load()
-	intro.framecount=0.00000000001
-	for i=0,119,1 do
-		if i >99 then
-			intro.cheve[i+1]=love.graphics.newImage("img/intro/Sequence 01"..tostring(i)..".jpg")
-		elseif i==0 then
-			intro.cheve[i+1]=love.graphics.newImage("img/intro/Sequence 0100"..tostring(i)..".jpg")
-		elseif i<10 then
-			intro.cheve[i+1]=love.graphics.newImage("img/intro/Sequence 0100"..tostring(i)..".jpg")
-		elseif i<100 then
-			intro.cheve[i+1]=love.graphics.newImage("img/intro/Sequence 010"..tostring(i)..".jpg")
-		end
-	end
+	intro.cheve=love.graphics.newImage("img/intro/Sequence 01100.jpg")
 end
 
 function intro.update(dt)
@@ -35,6 +24,7 @@ end
 
 function intro:update(dt)
 	if intro.framecount>590 then
+		intro.cheve=nil
 		Gamestate.switch(menu)
 	else
 		intro.framecount=intro.framecount+150*dt
@@ -42,8 +32,7 @@ function intro:update(dt)
 end
 
 function intro:draw()
-	frame = math.ceil(intro.framecount/5)
-	love.graphics.draw(intro.cheve[frame],0,0)
+	love.graphics.draw(intro.cheve,0,0)
 end 
 
 function intro:keypressed()
